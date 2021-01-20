a01fc9809ba8f71e3e1c1d53980f939da565d019
Logan Gillespie <leg16b@my.fsu.edu>
2021-01-20 12:42:33 -0500

    Update parser.c
    had erros with correctly updating everything .. this should have tilde expansion and pseudo code for part 6

--- parser.c
+++ parser.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-void prompt(); // logan added this for part 3
-void tilde_expansion(); //logan added this for part 4
+#include <stdbool.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
 typedef struct {
 	int size;
@@ -17,36 +17,49 @@ tokenlist *new_tokenlist(void);
 void add_token(tokenlist *tokens, char *item);
 void free_tokens(tokenlist *tokens);
 
+bool tokenCheck(char *token);
+bool dollarSignCheck(char *token);
+char *searchDollar(char *token);
+char* returnenv(char *token);
+void prompt ();
+
+bool hasTilde(char *token);
+void tildeExpansion(char *token);
+void externalCommmand();
 
 int main()
 {
-
 	while (1) {
-		printf("> ");
+		prompt();
 
 		/* input contains the whole command
 		 * tokens contains substrings from input split by spaces
 		 */
-
 		char *input = get_input();
 		printf("whole input: %s\n", input);
 
 		tokenlist *tokens = get_tokens(input);
+		//char *newToken, dollarCheck, command;
+
 		for (int i = 0; i < tokens->size; i++) {
 			printf("token %d: (%s)\n", i, tokens->items[i]);
+
+			char *newToken = tokens->items[i]; // grabs individual words from sentence
+			char* example = returnenv(newToken);
+			if(hasTilde(newToken) == true){
+				tildeExpansion(newToken);
+			}
+
+			printf("%s\n", example );
+
+
 		}
 
 		free(input);
 		free_tokens(tokens);
-
-		prompt(); // this is here as a test for part 3
-		tilde_expansion();
 	}
 
-	
 	return 0;
-
-	
 }
 
 tokenlist *new_tokenlist(void)
@@ -107,7 +120,7 @@ tokenlist *get_tokens(char *input)
 
 	char *tok = strtok(buf, " ");
 	while (tok != NULL) {
-		add_token(tokens, tok);
+		add_token(tokens, tok); //tok is individual word
 		tok = strtok(NULL, " ");
 	}
 
@@ -122,23 +135,60 @@ void free_tokens(tokenlist *tokens)
 
 	free(tokens);
 }
+bool tokenCheck(char *token){
+	char *check = getenv(token);
+//	printf("%sfunction tokencheck\n", check);
+	if(check != NULL)
+		return true;
+	return false;
+}
+bool dollarSignCheck(char *token){
+		if(token[0] == '$')
+			return true;
+		return false;
+}
+char *searchDollar(char* token){
+	size_t len = strlen(token);
+	char* substr;
+	for(int i = 0; i<len; i++){
+		if(token[i] == '$'){
+		//	printf("%sfunction SD \n", token);
+		//	printf("   ");
+			substr = token + 1;
+		//	printf("%sfunction SD \n", substr);
+		}
+	}
+	return substr;
+}
 
-
-
+char * returnenv(char * token){
+	char* hasDollar = token;
+	if(dollarSignCheck(token) == true){
+				hasDollar=searchDollar(token);
+				if(tokenCheck(hasDollar) == true){
+					hasDollar = getenv(hasDollar);
+					//printf("%s\n", hasDollar );
+					return hasDollar;
+				}
+			}
+			return hasDollar;
+}
 
 void prompt() // for part 3 of the project
 {
-	//my plan as of now is to wait til we have the env variables from part 1 set up and then just call the variable names instead of the hardcoded stuff i have now 
-	//also this might not end up being a void function
-
-	 printf(getenv("USER"));
-	 printf("@");
-	 printf(getenv("MACHINE"));
-	 printf(":");
-	 printf(getenv("PWD"));
-	 printf(">\n");
-
-
+    //my plan as of now is to wait til we have the env variables from part 1 set up and then just call the variable names instead of the hardcoded stuff i have now
+    //also this might not end up being a void function
+     printf(getenv("USER"));
+     printf("@");
+     printf(getenv("MACHINE"));
+     printf(":");
+     printf(getenv("PWD"));
+     printf(">\n");
+}
+bool hasTilde(char *token){ /*first checks to see if there is a tilde */
+	if(token[0] == '~')
+		return true;
+	return false;
 }
 void tildeExpansion(char *token){
 	printf(getenv("HOME"));
@@ -146,7 +196,6 @@ void tildeExpansion(char *token){
 		if(token[0] == '~'){
 			ntoken = token+1;
 		}
-
 	printf(ntoken);
 	printf("\n");
 
@@ -164,5 +213,212 @@ void externalCommmand()
 
 }
 
+typedef struct {
+	int size;
+	char **items;
+} tokenlist;
+
+char *get_input(void);
+tokenlist *get_tokens(char *input);
+
+tokenlist *new_tokenlist(void);
+void add_token(tokenlist *tokens, char *item);
+void free_tokens(tokenlist *tokens);
+
+bool tokenCheck(char *token);
+bool dollarSignCheck(char *token);
+char *searchDollar(char *token);
+char* returnenv(char *token);
+void prompt ();
+
+bool hasTilde(char *token);
+void tildeExpansion(char *token);
+void externalCommmand();
+
+int main()
+{
+	while (1) {
+		prompt();
+
+		/* input contains the whole command
+		 * tokens contains substrings from input split by spaces
+		 */
+		char *input = get_input();
+		printf("whole input: %s\n", input);
+
+		tokenlist *tokens = get_tokens(input);
+		//char *newToken, dollarCheck, command;
+
+		for (int i = 0; i < tokens->size; i++) {
+			printf("token %d: (%s)\n", i, tokens->items[i]);
+
+			char *newToken = tokens->items[i]; // grabs individual words from sentence
+			char* example = returnenv(newToken);
+			if(hasTilde(newToken) == true){
+				tildeExpansion(newToken);
+			}
+
+			printf("%s\n", example );
+
+
+		}
+
+		free(input);
+		free_tokens(tokens);
+	}
+
+	return 0;
+}
+
+tokenlist *new_tokenlist(void)
+{
+	tokenlist *tokens = (tokenlist *) malloc(sizeof(tokenlist));
+	tokens->size = 0;
+	tokens->items = (char **) malloc(sizeof(char *));
+	tokens->items[0] = NULL; /* make NULL terminated */
+	return tokens;
+}
+
+void add_token(tokenlist *tokens, char *item)
+{
+	int i = tokens->size;
+
+	tokens->items = (char **) realloc(tokens->items, (i + 2) * sizeof(char *));
+	tokens->items[i] = (char *) malloc(strlen(item) + 1);
+	tokens->items[i + 1] = NULL;
+	strcpy(tokens->items[i], item);
+
+	tokens->size += 1;
+}
+
+char *get_input(void)
+{
+	char *buffer = NULL;
+	int bufsize = 0;
+
+	char line[5];
+	while (fgets(line, 5, stdin) != NULL) {
+		int addby = 0;
+		char *newln = strchr(line, '\n');
+		if (newln != NULL)
+			addby = newln - line;
+		else
+			addby = 5 - 1;
+
+		buffer = (char *) realloc(buffer, bufsize + addby);
+		memcpy(&buffer[bufsize], line, addby);
+		bufsize += addby;
+
+		if (newln != NULL)
+			break;
+	}
+
+	buffer = (char *) realloc(buffer, bufsize + 1);
+	buffer[bufsize] = 0;
+
+	return buffer;
+}
+
+tokenlist *get_tokens(char *input)
+{
+	char *buf = (char *) malloc(strlen(input) + 1);
+	strcpy(buf, input);
+
+	tokenlist *tokens = new_tokenlist();
+
+	char *tok = strtok(buf, " ");
+	while (tok != NULL) {
+		add_token(tokens, tok); //tok is individual word
+		tok = strtok(NULL, " ");
+	}
+
+	free(buf);
+	return tokens;
+}
+
+void free_tokens(tokenlist *tokens)
+{
+	for (int i = 0; i < tokens->size; i++)
+		free(tokens->items[i]);
+
+	free(tokens);
+}
+bool tokenCheck(char *token){
+	char *check = getenv(token);
+//	printf("%sfunction tokencheck\n", check);
+	if(check != NULL)
+		return true;
+	return false;
+}
+bool dollarSignCheck(char *token){
+		if(token[0] == '$')
+			return true;
+		return false;
+}
+char *searchDollar(char* token){
+	size_t len = strlen(token);
+	char* substr;
+	for(int i = 0; i<len; i++){
+		if(token[i] == '$'){
+		//	printf("%sfunction SD \n", token);
+		//	printf("   ");
+			substr = token + 1;
+		//	printf("%sfunction SD \n", substr);
+		}
+	}
+	return substr;
+}
+
+char * returnenv(char * token){
+	char* hasDollar = token;
+	if(dollarSignCheck(token) == true){
+				hasDollar=searchDollar(token);
+				if(tokenCheck(hasDollar) == true){
+					hasDollar = getenv(hasDollar);
+					//printf("%s\n", hasDollar );
+					return hasDollar;
+				}
+			}
+			return hasDollar;
+}
+
+void prompt() // for part 3 of the project
+{
+    //my plan as of now is to wait til we have the env variables from part 1 set up and then just call the variable names instead of the hardcoded stuff i have now
+    //also this might not end up being a void function
+     printf(getenv("USER"));
+     printf("@");
+     printf(getenv("MACHINE"));
+     printf(":");
+     printf(getenv("PWD"));
+     printf(">\n");
+}
+bool hasTilde(char *token){ /*first checks to see if there is a tilde */
+	if(token[0] == '~')
+		return true;
+	return false;
+}
+void tildeExpansion(char *token){
+	printf(getenv("HOME"));
+		char *ntoken;
+		if(token[0] == '~'){
+			ntoken = token+1;
+		}
+	printf(ntoken);
+	printf("\n");
+
+}
+void externalCommmand()
+{
+	int pid= fork();
+	if(pid =0)
+	{
+		//use execv() command
+	}
+	else{
+		//in parent(main)
+	}
+
+}
 
 
